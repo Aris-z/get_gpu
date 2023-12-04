fba808c409fd7cb68551e8d89c240ffb06f0ae1a
arisyhzhang <arisyhzhang@tencent.com>
2023-12-04 16:40:15 +0800

    get gpu

---
+++ get_gpu.py
@@ -0,0 +1,44 @@
+import os
+import time
+
+
+def process(line):
+    return line.strip().split(',')
+
+# os.system('CUDA_VISIBLE_DEVICES=2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node 6 gpu.py')
+def query():
+    cmd = 'nvidia-smi --query-gpu=index,utilization.gpu --format=csv,noheader'
+    results = os.popen(cmd).readlines()
+    to_numberic = lambda v: int(v.upper().strip().replace('%',''))
+    results = [process(line) for line in results]
+    for i in range(len(results)):
+        results[i][1] = to_numberic(results[i][1])
+    return sorted(results,key=lambda d: d[1], reverse=False)
+
+flag = 0
+while True:
+    print("Querying...")
+    result = query()
+    i = 0
+    gpus = []
+    while i < len(result) and result[i][1] < 5:
+        gpus.append(result[i][0])
+        i += 1
+    if len(gpus) > 0:
+        if flag >= 2:
+            flag = 0
+        else:
+            flag += 1
+            continue
+        port = 30000 + len(gpus)
+        devices = ','.join(gpus)
+        cmd = f'CUDA_VISIBLE_DEVICES={devices} python -m torch.distributed.launch --nproc_per_node {len(gpus)} --master_port {port} gpu.py &'
+        # print(cmd)
+        try:
+            print(f"get gpu: {devices}")
+            os.system(cmd)
+        except:
+            pass
+    else:
+        flag = 0
+    time.sleep(300)
---
+++ gpu.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.distributed import DistributedSampler
+import os
+import time
+
+time_start = time.time()
+torch.distributed.init_process_group(backend="nccl")
+local_rank = torch.distributed.get_rank()
+torch.cuda.set_device(local_rank)
+device = torch.device("cuda", local_rank)
+
+size = 1024
+total_time = 30 #second
+time_need = False
+
+class randomdata(Dataset):
+    def __init__(self, size):
+        self.data = torch.randn(size, size, size).to('cuda')
+        self.len = 1
+    def __getitem__(self, index):
+        return self.data[index]
+    def __len__(self):
+        return self.len
+
+class get_gpu(nn.Module):
+    def __init__(self, input_size):
+        super().__init__()
+        self.fc = nn.Linear(input_size, input_size)
+    def forward(self, x):
+        for _ in range(1000):
+            y = self.fc(self.fc(x) @ self.fc(x))
+
+dataset = randomdata(size)
+
+data_load = DataLoader(dataset=dataset, batch_size=8, sampler=DistributedSampler(dataset))
+
+model = get_gpu(size).to(device)
+model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[local_rank], output_device=local_rank)
+for data in data_load:
+    while(True):
+        time_end = time.time()
+        if time_need and (time_end - time_start) > total_time:
+            exit()
+        model(data)
